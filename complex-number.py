69f5009692e60af4db58b723beb918759f9565fb
raoasifraza11 <raoasifraza1@hotmail.com>
2017-05-23 10:47:43 +0500

    Complex number example

---
+++ complex-number.py
@@ -0,0 +1,10 @@
+c1 = complex(4,5)
+print 'complex c1:', c1
+print
+c2 = complex(3, -2)
+print 'complex c2:',  c2
+print
+print 'sum of complex:', c1+c2              # addition
+print 'sum of complex:', c1/c2              # division
+print 'sum of complex:', c1*c2              # multiply
+
